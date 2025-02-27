8bd5fa2fa66b3e83dc9e38add7e8a29c0eed0cee
drawholka <mamrokhavlad@gmail.com>
2025-02-27 13:15:12 +0200

    added include header. left and right rotation methods

--- include/Tree.hpp
+++ include/Tree.hpp
@@ -1,17 +1,23 @@
 #ifndef TREE_HPP
 #define TREE_HPP
 
+#include "Node.hpp"
+
 template <typename T>
 class Tree
 {
+	Node<T>* head;
 
-	void left_rotate();
-	void right_rotation();
+	void left_rotate(Node<T>*);
+	void right_rotate(Node<T>*);
 
 public:
 	Tree(T);
 	void insert(T);
 	void remove(T);
+	int get_height(Node<T>*);
+	int get_balance(Node<T>*);
+
 
 };
 
